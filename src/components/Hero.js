e1e400f3cc99a52c92821944421ba5d87097f551
BraulioAR <braulior41@gmail.com>
2024-11-27 12:18:04 -0400

    test

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -35,9 +35,7 @@ export default function Hero() {
             <h1 className="mb-8 lg:text-balance text-5xl font-semibold tracking-tight text-gray-900 lg:text-7xl text-center">
               Daily Power
             </h1>
-            <h2 className="font-medium text-gray-500 lg:text-xl text-center ">
-              Representante exclusivo para República Dominicana de Baterías Aokly. Aplicaciones: Inversores, Paneles Solares y UPS.
-            </h2>
+            
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <Link
                 href="/Productos"
