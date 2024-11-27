0dc18b2f133b3daabd226d84019625a7848df94f
BraulioAR <braulior41@gmail.com>
2024-11-27 12:28:40 -0400

    test

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,21 +6,10 @@ import Link from "next/link";
 export default function Hero() {
 
   return (
-    <div className="bg-white">
-      <Header/>
-      <div className="relative lg:isolate px-6 pt-14 lg:px-8">
-        <div
-          aria-hidden="true"
-          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl lg:-top-80"
-        >
-          <div
-            style={{
-              clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-            }}
-            className=" relative w-[72.1875rem] aspect-[1155/678] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#EC7610] to-[#E73516] opacity-30 lg:left-[calc(50%-30rem)]"
-          />
-        </div>
+    <>
+    <Header/>
+      <div className="bg-white">
+      <div className="relative px-6 pt-14 lg:px-8">
         <div className="mx-auto max-w-7xl py-32 lg:py-56 animate-blurred-fade-in animate-duration-[300ms] animate-ease-in">
           <div className="hidden lg:mb-8 lg:flex lg:justify-center">
             <div className="relative rounded-full px-3 py-1 text-sm text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
@@ -51,19 +40,9 @@ export default function Hero() {
             </div>
           </div>
         </div>
-        <div
-          aria-hidden="true"
-          className="hidden lg:block absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl lg:top-[calc(100%-30rem)]"
-        >
-          <div
-            style={{
-              clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-            }}
-            className=" relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#EC7610] to-[#E73516] opacity-30 lg:left-[calc(50%+3rem)] lg:w-[72.1875rem]"
-          />
-        </div>
       </div>
     </div>
+    </>
+    
   )
 }
