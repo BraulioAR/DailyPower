bf6ae4672e3cdef3c8650263efc89c735eb8d43a
BraulioAR <braulior41@gmail.com>
2024-11-27 11:16:02 -0400

    Added backup font

--- src/app/page.js
+++ src/app/page.js
@@ -18,14 +18,14 @@ export default function Home() {
       <AboutUs />
       <NewProducts />
       <Clients />
-          <div className="w-full h-full bg-[#292929] relative z-40">
+          <section className="w-full h-full bg-[#292929] relative z-40">
       <div className="w-full h-full max-w-7xl mx-auto">
         <div className="w-full h-full flex flex-col lg:flex-row gap-4 lg:gap-0  items-center justify-between py-9">
         <h1 className="text-[#F5F5F5] font-bold text-4xl leading-[50.4px] text-center lg:text-start ">Para más información:</h1>
           <a href="/Contacto" className="border-[4px] border-[#E73516] bg-transparent text-[#E73516] hover:text-white hover:scale-105 hover:bg-[#E73516] py-3 text-[14px] font-bold leading-[23.8px] px-14">CONTÁCTENOS</a>
           </div>
           </div>
-      </div>
+      </section>
       </main>
   );
 }
