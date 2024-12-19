d8f6179c8b77f1a865123d6fa16546f41c344d3d
BraulioAR <braulior41@gmail.com>
2024-12-19 12:10:27 -0400

    Cache optimization

--- src/app/api/contentful/route.js
+++ src/app/api/contentful/route.js
@@ -89,6 +89,7 @@ export async function GET(request) {
 
     return new Response(JSON.stringify(items), {
       headers: { 'Content-Type': 'application/json' },
+      'Cache-Control': 'no-store'
     });
   } catch (error) {
     console.error('Error al obtener contenido:', error);
